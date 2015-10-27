d8cea062d9ac918186ad84a19ca95fa1802a1c8f
alainm <naarnet10@gmail.com>
2015-10-27 16:33:03 -0600

    Agregando libreria knp y wkhtmltox

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Gestoria\GestoriaBundle\GestoriaBundle(),
             new Ps\PdfBundle\PsPdfBundle(),
-//            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
+            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Gestoria\AdminBundle\AdminBundle(),
         );
